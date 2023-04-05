96ec8004151be97cf71d874062f575475b5acb45
Ria Kay <100300568+Ria-Kay@users.noreply.github.com>
2023-04-06 00:08:35 +0100

    Update login.php

--- groupsite/login.php
+++ groupsite/login.php
@@ -16,7 +16,7 @@
             //you could do other checks here
 
             //save to database (server host, username, password, database name)
-            $mysqli = mysqli_connect("localhost", "rk506_user", "Ayowhereyoureclothesat", "rk506_group_site");
+            $mysqli = mysqli_connect("localhost", "rk506_user", "NousSlayons", "rk506_ci527_test");
             
             //protection against SQL injection
             $firstname = mysqli_real_escape_string($mysqli, $firstname);
